65f40cc9db40b0521431b98f01e14b9e240037a9
akkez <me@akkez.ru>
2015-07-08 00:08:17 +0300

    about.

--- views/site/about.php
+++ views/site/about.php
@@ -5,12 +5,21 @@ use yii\helpers\Html;
 $this->title = 'About';
 ?>
 <div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
+	<h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        This is deepdream image online processor. You can upload any image and within a little time it will be processed and sent you via email. <br/>
+	<p>
+		This is deepdream image online processor. You can upload any image and within a little time it will be processed and sent you via email. <br/>
 		You can view <a href="/">image queue</a> or <a href="/gallery/index">already processed images</a>. <br/>
 		<br/><br/>
 		You also can always contact me via Telegram: <a href="https://telegram.me/akkez"><b>@akkez</b></a>
-    </p>
+	</p>
+	<br/><br/>
+
+	<p>More info about deepdream:
+	<ul>
+		<li><a target="_blank" href="http://googleresearch.blogspot.ru/2015/07/deepdream-code-example-for-visualizing.html">http://googleresearch.blogspot.ru/2015/07/deepdream-code-example-for-visualizing.html</a>
+		</li>
+		<li><a target="_blank" href="https://github.com/google/deepdream">https://github.com/google/deepdream</a></li>
+	</ul>
+	</p>
 </div>
