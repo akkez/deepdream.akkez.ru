d71dc3f14c834d6f3747f76567c8a721f28cdbd2
akkez <me@akkez.ru>
2015-07-08 14:24:34 +0300

    messenger!

--- views/site/about.php
+++ views/site/about.php
@@ -11,7 +11,7 @@ $this->title = 'About';
 		This is deepdream image online processor. You can upload any image and within a little time it will be processed and sent you via email. <br/>
 		You can view <a href="/">image queue</a> or <a href="/gallery/index">already processed images</a>. <br/>
 		<br/><br/>
-		You also can always contact me via Telegram: <a href="https://telegram.me/akkez"><b>@akkez</b></a>
+		You also can always contact me via Telegram messenger: <a href="https://telegram.me/akkez"><b>@akkez</b></a>
 	</p>
 	<br/><br/>
 
