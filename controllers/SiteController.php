28b358fb7155800bbbaef06a11dab3ad23c2a1e0
akkez <me@akkez.ru>
2015-08-23 21:36:27 +0300

    More pictures!

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -29,7 +29,7 @@ class SiteController extends Controller
 	{
 		$pendingImageCount = Picture::find()->where(['state' => 'new'])->count();
 		$pendingPictures   = Picture::find()->where(['state' => 'pending'])->orderBy('id ASC')->all();
-		$lastReady         = Picture::find()->where(['state' => 'ready'])->orderBy('likeCount DESC, id DESC')->limit(32)->all();
+		$lastReady         = Picture::find()->where(['state' => 'ready'])->orderBy('likeCount DESC, id DESC')->limit(100)->all();
 
 		return $this->render('index', [
 			'pendingImageCount' => $pendingImageCount,
